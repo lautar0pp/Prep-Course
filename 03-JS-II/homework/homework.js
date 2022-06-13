98f0e5bd4d5c2998c39bbe4ff43885fbb24dff78
Lautaro Ruben Leguizamon <lautaro_RL@outlook.com>
2022-06-13 19:04:16 -0300

    trabajos completos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+const { size } = require("@11ty/eleventy/src/TemplateCache");
+
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
@@ -173,26 +175,44 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if(valor === true )return 'Soy verdadero'
+  return 'Soy falso'
 }
+// al pasar un booleano como valor se podria hacer solamente if (valor)return soy verdadero
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+
+  let tablaDelSeis = [0]
+  for (let i = 1; i < 11; i++){
+    tablaDelSeis.push(6*i)
+  }
+  return tablaDelSeis
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   
+  var str = String(numero).length
+  if (str === 3)return true
+  return false
+
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+var contador = 0
+var suma = numero
+do {
+  suma = suma + 5
+  contador++
+}while(contador <8)
+  return suma
 }
 
 
